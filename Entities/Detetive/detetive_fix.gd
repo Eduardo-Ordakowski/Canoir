a8534177e20675e3d014f1dc3311a4190052ab46
Eduardo Ordakowski  - E. Desenvolvimento - Epays <eduardoordakowski@employer.com.br>
2026-05-21 01:39:18 -0300

    #canoir-19

--- Entities/Detetive/detetive_fix.gd
+++ Entities/Detetive/detetive_fix.gd
@@ -2,7 +2,19 @@ extends CharacterBody2D
 
 @export var speed = 200
 
+#region Variáveis
+
+var can_interact = false
+var interactable_object = null
+var opened_puzzle = false
+
+#endregion 
+
 func _physics_process(delta):
+	
+	if opened_puzzle:
+		return
+		
 	var input_direction = Vector2.ZERO
 	
 	if Input.is_action_pressed("walk_up"):
@@ -24,6 +36,9 @@ func _physics_process(delta):
 	move_and_slide()
 	_update_sprite_direction(input_direction)
 	
+	if Input.is_action_just_pressed("ui_accept") and can_interact:
+		open_puzzle()
+	
 func _update_sprite_direction(dir):
 	if dir.x != 0:
 		$AnimatedSprite2D.animation = "walk_side"
@@ -37,3 +52,21 @@ func _update_sprite_direction(dir):
 func start(pos):
 	global_position = pos
 	show()
+
+func open_puzzle():
+	if interactable_object:
+		opened_puzzle = true
+		$AnimatedSprite2D.stop()
+		interactable_object.activate_glitch(self)
+	
+func _on_interactive_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("glitch"):
+		can_interact = true
+		interactable_object = area.get_parent()
+		
+func _on_interactive_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("glitch"):
+		can_interact = false
+		interactable_object = null
+		
+		
